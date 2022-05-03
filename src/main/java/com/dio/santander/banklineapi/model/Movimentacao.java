2556162ed25948cad38a93fa4ab030341bfb7b39
Douglas-Dorug <douglasx30@gmail.com>
2022-05-03 20:45:17 -0300

    Mapeamente de entidades JPA

--- src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -1,12 +1,21 @@
 package com.dio.santander.banklineapi.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tab_movimentacao")
 public class Movimentacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "data_hora")
     private LocalDateTime dataHora;
     private String descricao;
     private Double valor;
+
+    @Enumerated(EnumType.STRING)
     private MovimentacaoTipo movimentacaoTipo;
 
     public Integer getId() {
