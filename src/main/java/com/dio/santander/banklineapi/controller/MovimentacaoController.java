6a7a102a8f17616152ebed0b10e7210a15a0f2c5
Douglas-Dorug <douglasx30@gmail.com>
2022-05-05 01:09:54 -0300

    Adicao de configuracao cors e listar movimentacao por id

--- src/main/java/com/dio/santander/banklineapi/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/banklineapi/controller/MovimentacaoController.java
@@ -25,6 +25,11 @@ public class MovimentacaoController {
         return movimentacaoRepository.findAll();
     }
 
+    @GetMapping("/{idConta}")
+    public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+        return movimentacaoRepository.findByIdConta(idConta);
+    }
+
     @PostMapping
     public void save(@RequestBody NovaMovimentacao novaMovimentacao){
         movimentacaoService.save(novaMovimentacao);
